6f79c7e21690bb43033383f72663f8ee67a35b7a
GracefulMan <41041321+GracefulMan@users.noreply.github.com>
2019-06-02 22:42:01 +0800

    fix bug for moitor

--- moitor/controller/ipApi.js
+++ moitor/controller/ipApi.js
@@ -1,12 +1,12 @@
 'use strict';
-const IpModel = require(__dirname+"/ipApi.js");
+const IpModel = require("./ipApi.js");
 const getIpByLocation = async ctx =>{
     let limit = ctx.query.limit || 10;
     let page = ctx.query.page || 1;
     let location  = ctx.query.location || -1;
     let skip = (page - 1) * limit;
     let ipList = await IpModel.getIp(limit,skip,location);
-    ctx.body = ipList
+    ctx.body = ipList;
     ctx.status = 200;
 };
 module.exports.routers = {
--- moitor/model/ipApi.js
+++ moitor/model/ipApi.js
@@ -1,5 +1,5 @@
 'use strict';
-const connection = require(__dirname+"/index");
+const connection = require("./index");
 
 const getIp =(limit,skip,location) =>{
     let _sql = `SELECT * FROM ip_information WHERE (location=${location} or ${location}=-1) ORDER BY updated DESC LIMIT ${skip},${limit};`;
