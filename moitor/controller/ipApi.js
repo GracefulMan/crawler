fea5e3db6abfb5e4638ea87a645850dd74037a77
GracefulMan <41041321+GracefulMan@users.noreply.github.com>
2019-06-04 18:37:34 +0800

    fix bug for moitor

--- moitor/controller/ipApi.js
+++ moitor/controller/ipApi.js
@@ -1,5 +1,5 @@
 'use strict';
-const IpModel = require("./ipApi.js");
+const IpModel = require("../model/ipApi");
 const getIpByLocation = async ctx =>{
     let limit = ctx.query.limit || 10;
     let page = ctx.query.page || 1;
