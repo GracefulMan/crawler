11d2e313b3227c2f5cdfdca607f0eefb64764415
GracefulMan <41041321+GracefulMan@users.noreply.github.com>
2019-06-04 18:38:38 +0800

    fix bug for moitor

--- moitor/model/ipApi.js
+++ moitor/model/ipApi.js
@@ -1,6 +1,5 @@
 'use strict';
-const connection = require("./index");
-
+const connection = require("../index");
 const getIp =(limit,skip,location) =>{
     let _sql = `SELECT * FROM ip_information WHERE (location=${location} or ${location}=-1) ORDER BY updated DESC LIMIT ${skip},${limit};`;
     return connection.query(_sql)
